53630274303b90097e30293b8d6cb49ee03d61f3
tage <763409282@qq.com>
2016-04-16 23:51:42 +0800

    Initial commit

---
+++ 1_urllib/test_1.py
@@ -0,0 +1,9 @@
+from urllib import request
+
+with request.urlopen('http://www.baidu.com') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data', data.decode('utf-8'))
+    print(type(f))
